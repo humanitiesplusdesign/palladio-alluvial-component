97aed8e0c9464cf7f0ca6e575396ee94f98c9e42
Ethan Jewett <esjewett@gmail.com>
2015-09-13 11:30:09 -0400

    Initial interactivity and highlighting, updates working

--- palladio-alluvial-component.js
+++ palladio-alluvial-component.js
@@ -28,7 +28,7 @@ angular.module('palladioAlluvial', ['palladio', 'palladio.services'])
 				post : function(scope, element, attrs) {
 					var margin = 5;
 					var columnWidth = 20;
-					var numValues = 20;
+					var numValues = 30;
 
 					var xfilter = dataService.getDataSync().xfilter;
 					var dimKeys = scope.dimensions.map(function(d) { return d.key; });
@@ -46,25 +46,24 @@ angular.module('palladioAlluvial', ['palladio', 'palladio.services'])
 							.order(function(d) { return d.exceptionCount; });
 					});
 
+					var linkKeys = [];
+					dimKeys.forEach(function(d, i) {
+						if(i < dimKeys.length-1) {
+							linkKeys.push([d, dimKeys[i+1]]);
+						}
+					});
+					var linkDimensions = linkKeys.map(function(k) {
+						return xfilter.dimension(function(d) { return [d[k[0]], d[k[1]]]; });
+					});
+					var linkGroups = linkDimensions.map(function(d) {
+						return reducer(d.group());
+					});
+
 					function addCounts(array) {
 						return array.reduce(function(a,b) {
 							return a + b.value.exceptionCount;
 						}, 0);
 					}
-
-					var columnOffsetScale = d3.scale.linear()
-							.domain([0, groups.length - 1])
-							.range([0 + margin, 1000 - margin*2 - columnWidth]);
-
-					var columnTotals = groups.map(function(g) {
-						return addCounts(g.all());
-					});
-
-					var columnScales = columnTotals.map(function(d) {
-						return d3.scale.linear()
-							.domain([0,d])
-							.range([0, scope.chartHeight - margin*2]);
-					});
 
 					function buildOffset(array, initial, attr, adj) {
 						attr = attr ? attr : 'offset';
@@ -91,92 +90,101 @@ angular.module('palladioAlluvial', ['palladio', 'palladio.services'])
 						return array;
 					}
 
-					var dataGroups = groups.map(function(d, i) {
-						var top = d.top(numValues);
-						var length = top.length;
-						return buildOffset(buildOther(top, columnTotals[i])).map(function(h) { h.index = i; h.length = length; return h; });
-					});
+					var columnOffsetScale = d3.scale.linear()
+							.domain([0, groups.length - 1])
+							.range([0 + margin, 1000 - margin*2 - columnWidth]);
 
-					var linkKeys = [];
-					dimKeys.forEach(function(d, i) {
-						if(i < dimKeys.length-1) {
-							linkKeys.push([d, dimKeys[i+1]]);
-						}
-					});
-					var linkDimensions = linkKeys.map(function(k) {
-						return xfilter.dimension(function(d) { return [d[k[0]], d[k[1]]]; });
-					});
-					var linkGroups = linkDimensions.map(function(d) {
-						return reducer(d.group());
-					});
-					
-					var linkMap = linkGroups.map(function() { return d3.map(); });
-					linkGroups.forEach(function(g,i) {
-						g.all().forEach(function(d) {
-							linkMap[i].set(d.key, d);
+					var columnTotals, columnScales, dataGroups, linkMaps, dataLinks;
+
+					var calcData = function() {
+						columnTotals = groups.map(function(g) {
+							return addCounts(g.all());
 						});
-					});
 
-					var dataLinks = linkGroups.map(function(d,i) {
-						var startKeys = dataGroups[i].map(function(c) { return { key: c.key, value: c.value.exceptionCount, offset: c.value.offset }; });
-						var endKeys = dataGroups[i+1].map(function(c) { return { key: c.key, value: c.value.exceptionCount, offset: c.value.offset }; });
+						columnScales = columnTotals.map(function(d) {
+							return d3.scale.linear()
+								.domain([0,d])
+								.range([0, scope.chartHeight - margin*2]);
+						});
 
-						var links = [];
+						dataGroups = groups.map(function(d, i) {
+							var top = d.top(numValues);
+							var length = top.length;
+							return buildOffset(buildOther(top, columnTotals[i])).map(function(h) { h.index = i; h.length = length; return h; });
+						});
 
-						startKeys.forEach(function(sk) {
-							var nodeLinks = [];
-
-							endKeys.forEach(function(ek) {
-								// Not all possible links actually exist
-								if(linkMap[i].has([sk.key,ek.key])) {
-									nodeLinks.push(linkMap[i].get([sk.key,ek.key]));
-								}
+						linkMaps = linkGroups.map(function() { return d3.map(); });
+						linkGroups.forEach(function(g,i) {
+							g.all().forEach(function(d) {
+								linkMaps[i].set(d.key, d);
 							});
-
-							nodeLinks.sort(function(a,b) { return b.value.exceptionCount - a.value.exceptionCount; });
-							var totalLinks = addCounts(nodeLinks);
-							var adj = 1;
-							if(totalLinks > sk.value) {
-								adj = sk.value / totalLinks;
-							}
-							nodeLinks.forEach(function(d) { d.value.sAdj = adj; });
-
-							buildOffset(nodeLinks, sk.offset, 'sOffset', adj);
-							links = links.concat(nodeLinks);
 						});
 
-						endKeys.forEach(function(ek) {
-							var nodeLinks = [];
+						dataLinks = linkGroups.map(function(d,i) {
+							var startKeys = dataGroups[i].map(function(c) { return { key: c.key, value: c.value.exceptionCount, offset: c.value.offset }; });
+							var endKeys = dataGroups[i+1].map(function(c) { return { key: c.key, value: c.value.exceptionCount, offset: c.value.offset }; });
+
+							var links = [];
 
 							startKeys.forEach(function(sk) {
-								if(linkMap[i].has([sk.key, ek.key])){
-									nodeLinks.push(linkMap[i].get([sk.key,ek.key]));
+								var nodeLinks = [];
+
+								endKeys.forEach(function(ek) {
+									// Not all possible links actually exist
+									if(linkMaps[i].has([sk.key,ek.key])) {
+										nodeLinks.push(linkMaps[i].get([sk.key,ek.key]));
+									}
+								});
+
+								nodeLinks.sort(function(a,b) { return b.value.exceptionCount - a.value.exceptionCount; });
+								var totalLinks = addCounts(nodeLinks);
+								var adj = 1;
+								if(totalLinks > sk.value) {
+									adj = sk.value / totalLinks;
 								}
+								nodeLinks.forEach(function(d) { d.value.sAdj = adj; });
+
+								buildOffset(nodeLinks, sk.offset, 'sOffset', adj);
+								links = links.concat(nodeLinks);
 							});
 
-							nodeLinks.sort(function(a,b) { return b.value.exceptionCount - a.value.exceptionCount; });
+							endKeys.forEach(function(ek) {
+								var nodeLinks = [];
 
-							var totalLinks = addCounts(nodeLinks);
-							var adj = 1;
-							if(totalLinks > ek.value) {
-								adj = ek.value / totalLinks;
-							}
-							nodeLinks.forEach(function(d) { d.value.tAdj = adj; });
+								startKeys.forEach(function(sk) {
+									if(linkMaps[i].has([sk.key, ek.key])){
+										nodeLinks.push(linkMaps[i].get([sk.key,ek.key]));
+									}
+								});
 
-							buildOffset(nodeLinks, ek.offset, 'tOffset', adj);
+								nodeLinks.sort(function(a,b) { return b.value.exceptionCount - a.value.exceptionCount; });
 
-							// Filter out links that are already recorded so we don't double-add.
-							nodeLinks.filter(function(d) { return links.indexOf(d) !== -1; });
+								var totalLinks = addCounts(nodeLinks);
+								var adj = 1;
+								if(totalLinks > ek.value) {
+									adj = ek.value / totalLinks;
+								}
+								nodeLinks.forEach(function(d) { d.value.tAdj = adj; });
 
-							links = links.concat(nodeLinks);
+								buildOffset(nodeLinks, ek.offset, 'tOffset', adj);
+
+								// Filter out links that are already recorded so we don't double-add.
+								nodeLinks.filter(function(d) { return links.indexOf(d) !== -1; });
+
+								links = links.concat(nodeLinks);
+							});
+
+							links.forEach(function(d) { d.index = i; });
+
+							return links;
 						});
+					};
 
-						links.forEach(function(d) { d.index = i; });
-
-						return links;
-					});
+					calcData();
 
 					element.height(scope.chartHeight);
+
+					var nodes, links;
 					
 					var svg = d3.select(element[0]).append('svg');
 					svg.attr('height', scope.chartHeight);
@@ -192,38 +200,12 @@ angular.module('palladioAlluvial', ['palladio', 'palladio.services'])
 						.append('g')
 							.attr('class', 'column');
 
-					columns.attr('transform', function(d, i) { return 'translate(' + columnOffsetScale(i) + ',0)'; });
-
-					var nodes = columns.selectAll('.node')
-							.data(function(d, i) { return d; });
-					
-					nodes.enter()
-						.append('rect')
-							.attr('class', 'node')
-							.attr('width', '20px')
-							.attr('height', function(d, i) {
-								if(i < d.length-1) {
-									return columnScales[d.index](d.value.exceptionCount)-1;
-								} else {
-									return columnScales[d.index](d.value.exceptionCount);
-								}
-							})
-							.attr('transform', function (d) { return 'translate(0,' + columnScales[d.index](d.value.offset) + ')'; })
-							.attr('fill', '#dddddd');
-
 					var linkColumns = g.selectAll('.link-column')
 							.data(linkKeys);
 
 					linkColumns.enter()
 						.append('g')
 							.attr('class', 'link-column');
-
-					linkColumns.attr('transform', function(d, i) { return 'translate(' + (columnOffsetScale(i)+columnWidth+2) + ',0)'; });
-
-					var links = linkColumns.selectAll('.link')
-							.data(function(d, i) {
-								return dataLinks[i];
-							});
 
 					var curvature = 0.5;
 					var linkWidth = columnOffsetScale(2) - columnOffsetScale(1) - columnWidth - 4;
@@ -247,13 +229,77 @@ angular.module('palladioAlluvial', ['palladio', 'palladio.services'])
 							" " + x0 + "," + sy2;
 					}
 
-					links.enter()
-						.append('path')
-							.attr('class', 'link')
-							.attr('d', link)
-							.style('opacity', "0.5")
-							.attr('fill', '#dddddd');
+					var update = function() {
+						calcData();
 
+						nodes = columns.selectAll('.node')
+							.data(function(d, i) { return dataGroups[i].filter(function(d) { return d.value.exceptionCount > 0; }); },
+									function(d, i) { return i + d.key; });
+					
+						nodes.enter()
+							.append('rect')
+								.attr('class', 'node')
+								.attr('width', '20px')
+								.attr('fill', '#dddddd')
+								.on('mouseover', function(d) {
+									d3.select(this)
+										.attr('fill', '#bbbbbb');
+									links.filter(function(l) {
+										return l.index === d.index && l.key[0] === d.key;
+									}).attr('fill', '#bbbbbb');
+									links.filter(function(l) {
+										return l.index === d.index-1 && l.key[1] === d.key;
+									}).attr('fill', '#bbbbbb');
+								})
+								.on('mouseout', function(d) {
+									d3.select(this)
+										.attr('fill', '#dddddd');
+									links.filter(function(l) {
+										return l.index === d.index && l.key[0] === d.key;
+									}).attr('fill', '#dddddd');
+									links.filter(function(l) {
+										return l.index === d.index-1 && l.key[1] === d.key;
+									}).attr('fill', '#dddddd');
+								});
+
+						nodes.exit().remove();
+
+						links = linkColumns.selectAll('.link')
+								.data(function(d, i) {
+									return dataLinks[i].filter(function(d) { return d.value.exceptionCount > 0; });
+								}, function(d, i) { return i + d.key.join(); });
+
+						links.enter()
+							.append('path')
+								.attr('class', 'link')
+								.style('opacity', "0.5")
+								.attr('fill', '#dddddd');
+
+						links.exit().remove();
+
+						columns.attr('transform', function(d, i) { return 'translate(' + columnOffsetScale(i) + ',0)'; });
+
+						linkColumns.attr('transform', function(d, i) { return 'translate(' + (columnOffsetScale(i)+columnWidth+2) + ',0)'; });
+
+						nodes.transition()
+								.attr('height', function(d, i) {
+									if(d.value.exceptionCount > 0) {
+										if(i < d.length-1) {
+											return columnScales[d.index](d.value.exceptionCount)-1;
+										} else {
+											return columnScales[d.index](d.value.exceptionCount);
+										}
+									}
+								})
+							.transition()
+								.attr('transform', function (d) { return 'translate(0,' + columnScales[d.index](d.value.offset) + ')'; });
+
+						links.attr('d', link);
+					};
+
+					update();
+
+					palladioService.onUpdate('alluvial', update);
 				}
 			}
 		};
